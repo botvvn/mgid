9028d856c8794b1448c1cb2beb8a9065256450c7
botvvn <44936151+botvvn@users.noreply.github.com>
2018-11-12 14:01:03 +0700

    Update mgid.js

--- mgid.js
+++ mgid.js
@@ -3,9 +3,3 @@ var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='sty
 var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M377486ScriptRootC296124")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
 catch(e){var iw=d;var c=d[gi]("M377486ScriptRootC296124");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=296124;c[ac](dv);
 var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/b/o/botv.vn.296124.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
-
-(function(){
-var D=new Date(),d=document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
-var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M377486ScriptRootC296129")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
-catch(e){var iw=d;var c=d[gi]("M377486ScriptRootC296129");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=296129;c[ac](dv);
-var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.mgid.com/b/o/botv.vn.296129.js?t="+D.getYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
